974c3968cb8b5cc91f464aba6dbd1bc3f16a9b08
Jony <jonathan.maldonado86@gmail.com>
2018-05-26 14:13:47 -0300

    Agrego estados y sus estados

---
+++ src/main/java/ar/com/sge/AhorroDeEnergia.java
@@ -0,0 +1,28 @@
+package ar.com.sge;
+
+public class AhorroDeEnergia extends Estado{
+
+	public AhorroDeEnergia(DispositivoInteligente unDispositivo) {
+		super(unDispositivo);
+		// TODO Auto-generated constructor stub
+	}
+
+	 
+	public void encender() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	 
+	public void apagar() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	 
+	public void ahorroDeEnergia() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
