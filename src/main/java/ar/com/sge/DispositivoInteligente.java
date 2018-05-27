599e5aa1f60bc227302d62594d6c1cf84b5fac22
juan sotelo <jsotelo@est.frba.utn.edu.ar>
2018-05-27 19:05:53 -0300

    Correcion calculo de consumo

--- src/main/java/ar/com/sge/DispositivoInteligente.java
+++ src/main/java/ar/com/sge/DispositivoInteligente.java
@@ -1,6 +1,7 @@
  package ar.com.sge;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -30,7 +31,14 @@ public class DispositivoInteligente {
 	public void setEstado(Estado estado) {
  		this.estado = estado;
 	}
-	
+	public int getKwPorHora() {
+		return kwPorHora;
+	}
+
+	public void setKwPorHora(int kwPorHora) {
+		this.kwPorHora = kwPorHora;
+	}
+
 	// Cambios de estados
 	public void encender(){
 		this.estado.encender();
@@ -54,27 +62,31 @@ public class DispositivoInteligente {
 	}
 	
 	// Calculo de consumos
-	public void tiempoDeCarga(int _tiempo) {
-		this.kwConsumido += (kwPorHora/60) * _tiempo;
-	}
-	public void consumidoUltimasNhoras (int cantHoras) {
-		LocalDateTime fecha = LocalDateTime.now().minusHours(cantHoras);	
+	
+	public float consumidoUltimasNhoras (int cantHoras) {
+		LocalDateTime fechaInicio = LocalDateTime.now().minusHours(cantHoras);
+		LocalDateTime fechaFin = LocalDateTime.now();
+		return this.consumidoEntre(fechaInicio, fechaFin);
 	}
 	public float consumidoEntre (LocalDateTime fechaInicio , LocalDateTime fechaFin) {
 		List<Estado> lstEstadosEntre;
 		lstEstadosEntre = 
 				(List<Estado>) this.listaDeEstados.stream().filter(e -> e.getInicio().isAfter(fechaInicio) && e.getFin().isBefore(fechaFin));
-		return this.consumoTotal(lstEstadosEntre);
+		return this.totalDeHoras(lstEstadosEntre) * this.getKwPorHora();
 		 
 	}
-	public float consumoTotal (List<Estado> lstEstados) {		
-		float consumoTotalestados = 0;		
+	public float totalDeHoras (List<Estado> lstEstados) {		
+		float totalHoras = 0;		
 		for (Estado estado : lstEstados){
-			consumoTotalestados += estado.getConsumo();
+			totalHoras += diferenciaHoras(estado.getFin(),estado.getInicio());
 		}
-		return consumoTotalestados;
+		return totalHoras;
 	}
 	
+	public static int diferenciaHoras(LocalDateTime unahora, LocalDateTime otrahora){
+		int dif= (int) ChronoUnit.HOURS.between(unahora, otrahora);
+		return dif;
+	}
 	//VA?????
 	
 	public void agregarAListaDeEstados(Estado unEstado) {
